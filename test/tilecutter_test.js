ed03225d552b544b498572afd98f0264e17131cb
James Lawson <github@escape-committee.com>
2015-01-28 13:23:07 +0000

    Reduced image file sizes

--- test/tilecutter_test.js
+++ test/tilecutter_test.js
@@ -8,32 +8,26 @@ exports.tilecutter = {
         done();
     },
     default_options: function (test) {
-        test.expect(7);
+        test.expect(5);
 
         var actual = gdal.open('tmp/source-4096x4096-default.tif');
-        var expected = gdal.open('test/expected/source-4096x4096-default.tif');
 
-        test.equal(actual.geoTransform, expected.geoTransform, 'default translated file has correct geoTransform.');
-        test.equal(actual.srs, expected.srs, 'default translated file has correct srs.');
-        test.equal(actual.rasterSize.x, expected.rasterSize.x, 'default translated file has correct x-scale rasterSize.');
-        test.equal(actual.rasterSize.y, expected.rasterSize.y, 'default translated file has correct y-scale rasterSize.');
-        test.equal(actual.bands.count(), expected.bands.count(), 'default translated file has correct bands.');
-        test.equal(actual.layers.count(), expected.layers.count(), 'default translated file has correct layers.');
+        test.equal(actual.rasterSize.x, 4096, 'default translated file has correct x-scale rasterSize.');
+        test.equal(actual.rasterSize.y, 4096, 'default translated file has correct y-scale rasterSize.');
+        test.equal(actual.bands.count(), 3, 'default translated file has correct bands.');
+        test.equal(actual.layers.count(), 0, 'default translated file has correct layers.');
         test.equal(actual.description, 'tmp/source-4096x4096-default.tif', 'default translated file has correct description.');
         test.done();
     },
     custom_options: function (test) {
-        test.expect(7);
+        test.expect(5);
 
         var actual = gdal.open('tmp/source-4096x4096-custom.tif');
-        var expected = gdal.open('test/expected/source-4096x4096-custom.tif');
 
-        test.equal(actual.geoTransform, expected.geoTransform, 'default translated file has correct geoTransform.');
-        test.equal(actual.srs, expected.srs, 'default translated file has correct srs.');
-        test.equal(actual.rasterSize.x, expected.rasterSize.x, 'default translated file has correct x-scale rasterSize.');
-        test.equal(actual.rasterSize.y, expected.rasterSize.y, 'default translated file has correct y-scale rasterSize.');
-        test.equal(actual.bands.count(), expected.bands.count(), 'default translated file has correct bands.');
-        test.equal(actual.layers.count(), expected.layers.count(), 'default translated file has correct layers.');
+        test.equal(actual.rasterSize.x, 4096, 'default translated file has correct x-scale rasterSize.');
+        test.equal(actual.rasterSize.y, 4096, 'default translated file has correct y-scale rasterSize.');
+        test.equal(actual.bands.count(), 3, 'default translated file has correct bands.');
+        test.equal(actual.layers.count(), 0, 'default translated file has correct layers.');
         test.equal(actual.description, 'tmp/source-4096x4096-custom.tif', 'default translated file has correct description.');
         test.done();
     }
